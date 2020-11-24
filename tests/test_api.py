9f56c61188ec15fa1fe8f219e328e06ce2ac3cb0
iuvbio <lyndonbjohn@gmail.com>
2020-11-24 02:25:36 +0100

    add rest api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,11 @@
 import logging
 
 import pytest
+import requests_mock
 
-from tests.conftest import log_has
+from tests.conftest import log_has, register_resource
 from pycwatch.errors import APIError
-from pycwatch.rest import RestAPI, KEY_HEADER
+from pycwatch.rest import Allowance, RestAPI, KEY_HEADER
 
 
 def test_key_setter(api_key):
@@ -25,14 +26,56 @@ def test_init_with_key(api_key):
     assert api.client.headers[KEY_HEADER] == api_key
 
 
-def test_perform_request():
-    pass
-
-
-def test_perform_request_no_key(caplog):
+def test_perform_request_no_key(caplog, mock_resource):
     caplog.set_level(logging.INFO)
     api = RestAPI()
     line = "API Key needs to be set"
     with pytest.raises(APIError):
-        assets = api.list_assets()
+        api.perform_request(mock_resource)
         assert log_has(line, caplog)
+
+
+@requests_mock.Mocker(kw='rmock')
+def test_perform_request(mocker, api_key, mock_resource, **kwargs):
+    api = RestAPI(api_key)
+    mocker.patch("pycwatch.rest", api)
+    resource = api.client.get_resource(mock_resource)
+    response_expected = {
+        "result": {
+            "the-result": [1, 2, 3, 4, 5]
+        }
+    }
+    update_allowance_mock = mocker.MagicMock()
+    mocker.patch("pycwatch.rest.update_allowance", update_allowance_mock)
+    register_resource(kwargs['rmock'], resource, 'GET', 200,
+                      json=response_expected)
+    result = api.perform_request(mock_resource)
+    assert result == response_expected['result']
+    update_allowance_mock.assert_called_once()
+
+
+@requests_mock.Mocker(kw='rmock')
+def test_update_allowance(mocker, api_key, mock_resource, **kwargs):
+    api = RestAPI(api_key)
+    mocker.patch("pycwatch.rest", api)
+    resource = api.client.get_resource(mock_resource)
+    response_expected = {
+        "allowance": {
+            "cost": 0.015,
+            "remaining": 9.985,
+            "upgrade": "Check https://cryptowat.ch"
+        },
+        "result": {}
+    }
+    allowance = Allowance(response_expected)
+    register_resource(kwargs['rmock'], resource, 'GET', 200,
+                      json=response_expected)
+    _ = api.perform_request(mock_resource)
+    assert str(allowance) == str({
+            'last_request_cost': 0.015,
+            'remaining': 9.985,
+            'remaining_paid': None
+    })
+    assert str(api.allowance) == str(allowance)
+    assert api.allowance.cost == 0.015
+    assert api.allowance.remaining == 9.985
