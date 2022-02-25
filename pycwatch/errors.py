50d02077bd32a016f24d049a55150fff7e358c5f
ljnsn <lbj@goja.me>
2022-02-25 03:15:10 +0100

    🚨 fix typing on errors

--- pycwatch/errors.py
+++ pycwatch/errors.py
@@ -1,20 +1,24 @@
+from typing import Any, Optional
+
+
 class CryptowatchError(Exception):
     def __init__(
         self,
-        message=None,
-        http_body=None,
-        http_status=None,
-        json_body=None,
-        headers=None,
-    ):
+        message: Optional[str] = None,
+        http_body: Optional[str] = None,
+        http_status: Optional[int] = None,
+        json_body: Optional[dict[str, Any]] = None,
+        headers: Optional[dict[str, Any]] = None,
+    ) -> None:
         super().__init__(message)
 
         self._message = message
         self.http_body = http_body
         self.http_status = http_status
+        self.json_body = json_body
         self.headers = headers or {}
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "{}(message={}, http_status={})".format(
             self.__class__.__name__, self._message, self.http_status
         )
