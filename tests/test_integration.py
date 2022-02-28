9fa62741e9fca13cd07f415a134bba9bbb3a8f7c
ljnsn <lbj@goja.me>
2022-02-28 19:22:04 +0100

    ✅ Add remaining tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -95,13 +95,13 @@ def test_get_pair(cassette: Cassette, live_client: CryptoWatchClient):
         pair = live_client.get_pair(pair="aaa")
 
 
-@with_cassette("list_exchanges")
+@with_cassette("list_exchanges.yml")
 def test_list_exchanges(cassette: Cassette, live_client: CryptoWatchClient):
     exchanges = live_client.list_exchanges()
     assert exchanges == Response[models.ExchangeList](**load_response(cassette, 0))
 
 
-@with_cassette("get_exchange")
+@with_cassette("get_exchange.yml")
 def test_get_exchange(cassette: Cassette, live_client: CryptoWatchClient):
     exchanges = ["kraken", "binance", "bittrex"]
     for i, ex in enumerate(exchanges):
@@ -110,3 +110,147 @@ def test_get_exchange(cassette: Cassette, live_client: CryptoWatchClient):
 
     with pytest.raises(ClientError, match="404 Error: Not Found"):
         exchange = live_client.get_exchange(exchange="aaa")
+
+
+@with_cassette("list_markets.yml")
+def test_list_markets(cassette: Cassette, live_client: CryptoWatchClient):
+    markets = live_client.list_markets()
+    assert markets == PaginatedResponse[models.MarketList](**load_response(cassette, 0))
+
+
+@with_cassette("get_market.yml")
+def test_get_market(cassette: Cassette, live_client: CryptoWatchClient):
+    markets = [
+        ("kraken", "btceur"),
+        ("binance", "ethbtc"),
+        ("kraken", "ltcbtc"),
+        ("bittrex", "neoeth"),
+    ]
+    for i, m in enumerate(markets):
+        market = live_client.get_market(exchange=m[0], pair=m[1])
+        assert market == Response[models.Market](**load_response(cassette, i))
+
+    with pytest.raises(ClientError, match="404 Error: Not Found"):
+        market = live_client.get_market(exchange="kraken", pair="aaabbb")
+
+
+@with_cassette("get_market_price.yml")
+def test_get_market_price(cassette: Cassette, live_client: CryptoWatchClient):
+    markets = [
+        ("kraken", "btceur"),
+        ("binance", "ethbtc"),
+        ("kraken", "ltcbtc"),
+        ("bittrex", "neoeth"),
+    ]
+    for i, m in enumerate(markets):
+        price = live_client.get_market_price(exchange=m[0], pair=m[1])
+        assert price == Response[models.MarketPrice](**load_response(cassette, i))
+
+    with pytest.raises(ClientError, match="404 Error: Not Found"):
+        market = live_client.get_market_price(exchange="kraken", pair="aaabbb")
+
+
+@with_cassette("get_market_trades.yml")
+def test_get_market_trades(cassette: Cassette, live_client: CryptoWatchClient):
+    markets = [
+        ("kraken", "btceur"),
+        ("binance", "ethbtc"),
+        ("kraken", "ltcbtc"),
+        ("bittrex", "neoeth"),
+    ]
+    for i, m in enumerate(markets):
+        trades = live_client.get_market_trades(exchange=m[0], pair=m[1])
+        assert trades == Response[models.MarketTradeList](**load_response(cassette, i))
+
+    with pytest.raises(ClientError, match="404 Error: Not Found"):
+        trades = live_client.get_market_trades(exchange="kraken", pair="aaabbb")
+
+
+@with_cassette("get_market_summary.yml")
+def test_get_market_summary(cassette: Cassette, live_client: CryptoWatchClient):
+    markets = [
+        ("kraken", "btceur"),
+        ("binance", "ethbtc"),
+        ("kraken", "ltcbtc"),
+        ("bittrex", "neoeth"),
+    ]
+    for i, m in enumerate(markets):
+        summary = live_client.get_market_summary(exchange=m[0], pair=m[1])
+        assert summary == Response[models.MarketSummary](**load_response(cassette, i))
+
+    with pytest.raises(ClientError, match="404 Error: Not Found"):
+        summary = live_client.get_market_summary(exchange="kraken", pair="aaabbb")
+
+
+@with_cassette("get_market_order_book.yml")
+def test_get_market_order_book(cassette: Cassette, live_client: CryptoWatchClient):
+    markets = [
+        ("kraken", "btceur"),
+        ("binance", "ethbtc"),
+        ("kraken", "ltcbtc"),
+        ("bittrex", "neoeth"),
+    ]
+    for i, m in enumerate(markets):
+        orderbook = live_client.get_market_order_book(exchange=m[0], pair=m[1])
+        assert orderbook == Response[models.OrderBook](**load_response(cassette, i))
+
+    with pytest.raises(ClientError, match="404 Error: Not Found"):
+        orderbook = live_client.get_market_order_book(exchange="kraken", pair="aaabbb")
+
+
+@with_cassette("get_market_order_book_liquidity.yml")
+def test_get_market_order_book_liquidity(
+    cassette: Cassette, live_client: CryptoWatchClient
+):
+    markets = [
+        ("kraken", "btceur"),
+        ("binance", "ethbtc"),
+        ("kraken", "ltcbtc"),
+        ("bittrex", "neoeth"),
+    ]
+    for i, m in enumerate(markets):
+        orderbook = live_client.get_market_order_book_liquidity(
+            exchange=m[0], pair=m[1]
+        )
+        assert orderbook == Response[models.OrderBookLiquidity](
+            **load_response(cassette, i)
+        )
+
+    with pytest.raises(ClientError, match="404 Error: Not Found"):
+        orderbook = live_client.get_market_order_book_liquidity(
+            exchange="kraken", pair="aaabbb"
+        )
+
+
+@with_cassette("get_all_market_prices.yml")
+def test_get_all_market_prices(cassette: Cassette, live_client: CryptoWatchClient):
+    prices = live_client.get_all_market_prices()
+    assert prices == PaginatedResponse[models.AllPrices](**load_response(cassette, 0))
+
+
+@with_cassette("get_all_market_summaries.yml")
+def test_get_all_market_summaries(cassette: Cassette, live_client: CryptoWatchClient):
+    summaries = live_client.get_all_market_summaries()
+    assert summaries == Response[models.AllSummaries](**load_response(cassette))
+
+
+@with_cassette("calculate_quote.yml")
+def test_calculate_quote(cassette: Cassette, live_client: CryptoWatchClient):
+    quote = live_client.calculate_quote(exchange="kraken", pair="btceur", amount=10)
+    assert quote == Response[models.OrderBookCalculator](**load_response(cassette, 0))
+
+
+@with_cassette("list_exchange_markets.yml")
+def test_list_exchange_markets(cassette: Cassette, live_client: CryptoWatchClient):
+    exchanges = [
+        "kraken",
+        "binance",
+        "kraken",
+        "bittrex",
+    ]
+    for i, exchange in enumerate(exchanges):
+        markets = live_client.list_exchange_markets(exchange=exchange)
+        assert markets == Response[models.ExchangeMarkets](**load_response(cassette, i))
+
+    with pytest.raises(ClientError, match="404 Error: Not Found"):
+        markets = live_client.list_exchange_markets(exchange="aabbbaba")
