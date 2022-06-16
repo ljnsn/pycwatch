5644fe70090772a6877abaf788a964683810956a
ljnsn <lbj@goja.me>
2022-06-16 22:04:38 +0200

    :white_check_mark: Add test for new OHLCV query params

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,25 @@
+from typing import List, Optional, Union
+
 import pytest
 
 from pycwatch.models import OHLCVQueryParams
 
 
-def test_ohlcv_list_to_str():
-    periods = [60, 180, 300]
-    lstring = OHLCVQueryParams.list_to_str(periods)
-    assert lstring == "60,180,300"
-    assert OHLCVQueryParams.list_to_str([]) is None
+@pytest.mark.parametrize(
+    ["periods", "expected"],
+    [
+        ([], None),
+        ([60, 180, 300], "60,180,300"),
+        ([60, 180, 604800, "604800_Monday"], "60,180,604800,604800_Monday"),
+        (["1m", "30m", "1w_monday"], "60,1800,604800_Monday"),
+        ([60, "30m"], "60,1800"),
+        (["3m", 180], "180"),
+    ],
+)
+def test_ohlcv_query_params(periods: List[Union[str, int]], expected: Optional[str]):
+    params = OHLCVQueryParams(periods=periods)
 
-    with pytest.raises(TypeError, match="all periods must be integers"):
-        OHLCVQueryParams.list_to_str(["60", "180", "300"])
+    assert params.periods == expected
+
+    with pytest.raises(ValueError, match="Invalid period label"):
+        OHLCVQueryParams(periods=["60", "180", "300"])
