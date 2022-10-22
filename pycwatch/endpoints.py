a24f08108ea13d64432268f82700f3b7c8aebeeb
ljnsn <82611987+ljnsn@users.noreply.github.com>
2022-10-22 19:55:18 +0200

    :bulb: Add docstring to Endpoint.

--- pycwatch/endpoints.py
+++ pycwatch/endpoints.py
@@ -3,6 +3,8 @@ from apiclient import endpoint
 
 @endpoint(base_url="https://api.cryptowat.ch")
 class Endpoint:
+    """The endpoints supported by this client."""
+
     root = "/"
     list_assets = "/assets"
     asset_detail = "/assets/{assetCode}"
