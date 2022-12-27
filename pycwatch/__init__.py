5940c02b513badf2e6be76a79ede94c892983b39
ljnsn <82611987+ljnsn@users.noreply.github.com>
2022-12-27 21:23:41 +0100

    🐛 fix: use importlib.metadata to get version

--- pycwatch/__init__.py
+++ pycwatch/__init__.py
@@ -1,3 +1,12 @@
-__version__ = "1.1.1"
+import sys
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
 
 from .client import CryptoWatchClient
+
+__version__ = version("pycwatch")
+
+__all__ = ("CryptoWatchClient",)
