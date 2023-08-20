109f136cc270560daeea7466b1560c5e117743c5
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-20 12:48:55 +0200

    🐛 fix(config): make settings attrs class

--- pycwatch/config.py
+++ pycwatch/config.py
@@ -1,13 +1,21 @@
 """Module holding configuration."""
+
+import os
 from typing import Optional
 
-from pydantic import BaseSettings
+import attrs
 
 
-class Settings(BaseSettings):
+@attrs.define()
+class Settings:
     """Settings for the client."""
 
     CW_API_KEY: Optional[str] = None
+
+    def __attrs_post_init__(self) -> None:
+        """Post init hook."""
+        if self.CW_API_KEY is None:
+            self.CW_API_KEY = os.getenv("CW_API_KEY")
 
 
 settings = Settings()
