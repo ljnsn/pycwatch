7cd56bcdb88fb37e5f77d381ebf73ad9123a5152
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-20 02:33:03 +0200

    ✅ test(response-handler): add tests for ujson response handler

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,8 @@
+import pytest
+from apiclient.exceptions import ResponseParseError
+from apiclient.response import Response
 from pycwatch import CryptoWatchClient
+from pycwatch.client import UJSONResponseHandler
 
 
 def test_init_with_key(api_key: str) -> None:
@@ -6,3 +10,36 @@ def test_init_with_key(api_key: str) -> None:
     api = CryptoWatchClient(api_key)
     assert api._api_key == api_key
     assert api.is_authenticated
+
+
+class FakeResponse(Response):
+    """A fake response for testing."""
+
+    def __init__(self, content: str) -> None:
+        self.content = content
+
+    def get_raw_data(self) -> str:
+        """Return the raw data."""
+        return self.content
+
+
+def test_ujson_response_handler_no_data() -> None:
+    """Verify an empty response returns None."""
+    response = FakeResponse("")
+
+    assert UJSONResponseHandler.get_request_data(response) is None
+
+
+def test_ujson_response_handler_good_data() -> None:
+    """Verify a good response is loaded correctly."""
+    response = FakeResponse('{"foo": "bar"}')
+
+    assert UJSONResponseHandler.get_request_data(response) == {"foo": "bar"}
+
+
+def test_ujson_response_handler_bad_data() -> None:
+    """Verify a malformed response raises an error."""
+    response = FakeResponse("foo")
+
+    with pytest.raises(ResponseParseError):
+        UJSONResponseHandler.get_request_data(response)
