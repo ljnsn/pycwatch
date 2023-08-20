03b0f386ba1c1b2a939e0e4fd0c0a6e01a1d1a3f
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-21 01:08:47 +0200

    🔨 script(lint): check packages with mypy instead of paths

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 set -e
+
+mypy_params=()
+for directory in workspaces/*; do
+  # Check if the current item is a directory
+  if [[ -d "$directory" ]]; then
+    dir_name=$(basename "$directory")
+    mypy_params+=("-p" "pycwatch.$dir_name")
+  fi
+done
+
 set -x
 
 black workspaces --check
 ruff workspaces
-mypy workspaces --explicit-package-bases
+mypy "${mypy_params[@]}"
