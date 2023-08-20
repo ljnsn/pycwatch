adac929fcca3fc39b1b29e19be48ab86a20496e9
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-21 01:58:30 +0200

    ✨ feat(cli): add pairs command

--- workspaces/cli/src/pycwatch/cli/main.py
+++ workspaces/cli/src/pycwatch/cli/main.py
@@ -4,12 +4,14 @@ import typer
 from rich import print
 
 from pycwatch.cli.assets import app as assets_app
+from pycwatch.cli.pairs import app as pairs_app
 from pycwatch.cli.utils import get_client
 from pycwatch.lib import CryptoWatchClient
 from pycwatch.lib.conversion import converter
 
 app = typer.Typer(name="PyCwatch CLI")
 app.add_typer(assets_app)
+app.add_typer(pairs_app)
 
 
 @app.command()
