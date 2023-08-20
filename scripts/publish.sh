89307467e1dd464fcb6806137449a03a99eafce7
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-20 20:32:43 +0200

    🔨 script(main): replace version and path dependencies in publishs script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-package=$1
+package=workspaces/$1
 
 # get the current version of the main package
 version=$(grep -m 1 version pyproject.toml | tr -s ' ' | tr -d '"' | tr -d "'" | cut -d' ' -f3)
 
 # replace version in pyproject.toml
 sed -i "s/^version = \".*\"/version = \"$version\"/" "$package/pyproject.toml"
+
+# replace all path dependencies
+sed -i "s/^\(pycwatch-.*\) = { path = \".*\" }/\1 = \"$version\"/" "$package/pyproject.toml"
 
 # poetry publish --build
 cd "$package" && poetry build
