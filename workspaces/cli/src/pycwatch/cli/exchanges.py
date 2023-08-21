3a2590c96f2e7195b7b00b670e38d9b5675a74fd
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-21 21:31:21 +0200

    ✨ feat(cli): add exchanges commands

---
+++ workspaces/cli/src/pycwatch/cli/exchanges.py
@@ -0,0 +1,38 @@
+"""Commands related to exchanges."""
+
+import typer
+from rich import print
+
+from pycwatch.cli.utils import get_client
+from pycwatch.lib.conversion import converter
+
+# ruff: noqa: B008
+
+app = typer.Typer(name="exchanges", help="Get or list exchanges.")
+
+
+@app.command(name="list")
+def list_exchanges(ctx: typer.Context) -> None:
+    """List exchanges."""
+    response = get_client(ctx).list_exchanges()
+    print(converter.unstructure(response.result))
+
+
+@app.command(name="get")
+def get_exchange(
+    ctx: typer.Context,
+    code: str = typer.Argument(..., help="Exchange code."),
+) -> None:
+    """Get exchange."""
+    result = get_client(ctx).get_exchange(code)
+    print(converter.unstructure(result))
+
+
+@app.command(name="markets")
+def list_exchange_markets(
+    ctx: typer.Context,
+    code: str = typer.Argument(..., help="Exchange code."),
+) -> None:
+    """List exchange markets."""
+    result = get_client(ctx).list_exchange_markets(code)
+    print(converter.unstructure(result))
