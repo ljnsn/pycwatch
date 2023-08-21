a83fb5c5e5dd22235454f5e9bb9e06398ffe529b
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-21 19:52:34 +0200

    ✨ feat(lib): add markets cli

--- workspaces/cli/src/pycwatch/cli/main.py
+++ workspaces/cli/src/pycwatch/cli/main.py
@@ -4,6 +4,7 @@ import typer
 from rich import print
 
 from pycwatch.cli.assets import app as assets_app
+from pycwatch.cli.markets import app as markets_app
 from pycwatch.cli.pairs import app as pairs_app
 from pycwatch.cli.utils import get_client
 from pycwatch.lib import CryptoWatchClient
@@ -12,6 +13,7 @@ from pycwatch.lib.conversion import converter
 app = typer.Typer(name="PyCwatch CLI")
 app.add_typer(assets_app)
 app.add_typer(pairs_app)
+app.add_typer(markets_app)
 
 
 @app.command()
