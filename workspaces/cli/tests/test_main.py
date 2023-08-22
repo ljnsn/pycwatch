cc789628fd79242c7a9c2cf4054f881714b42d6c
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-23 01:32:20 +0200

    ✅ test(cli): add some tests for formatting

--- workspaces/cli/tests/test_main.py
+++ workspaces/cli/tests/test_main.py
@@ -1,9 +1,11 @@
+import pytest
 from typer.testing import CliRunner
 
 from pycwatch.cli.main import app
 
 
-def test_info(runner: CliRunner) -> None:
+@pytest.mark.parametrize("style", ["records", "objects", "json", "csv", "table"])
+def test_info(runner: CliRunner, style: str) -> None:
     """Verify that the info command works."""
-    result = runner.invoke(app, ["info"])
+    result = runner.invoke(app, ["info", "-f", style])
     assert result.exit_code == 0
