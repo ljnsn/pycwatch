25836eaf3d142b1d35f58163557d481baab0c227
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-08-23 02:20:29 +0200

    ✅ test(cli): add a test for the version flag

--- workspaces/cli/tests/test_main.py
+++ workspaces/cli/tests/test_main.py
@@ -9,3 +9,9 @@ def test_info(runner: CliRunner, style: str) -> None:
     """Verify that the info command works."""
     result = runner.invoke(app, ["info", "-f", style])
     assert result.exit_code == 0
+
+
+def test_version(runner: CliRunner) -> None:
+    """Verify that the version flag works."""
+    result = runner.invoke(app, ["--version"])
+    assert result.exit_code == 0
